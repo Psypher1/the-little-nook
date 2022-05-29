a78b1a481102bf55ddaa5e9c6bcb320c34ad6683
James 'Dante' Midzi <jamesmidzi@gmail.com>
2022-05-29 21:28:32 +0200

    updated files, data pulled successfully

--- bookstore/schemas/author.js
+++ bookstore/schemas/author.js
@@ -1,37 +1,36 @@
 export default {
-    name: "author",
-    title: "Author",
-    type: "document",
-    fields: [
-      {
-        name: "name",
-        title: "Name",
-        type: "string",
+  name: "author",
+  title: "Author",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "name",
+        maxLength: 96,
       },
-      {
-        name: "slug",
-        title: "Slug",
-        type: "slug",
-        options: {
-          source: "name",
-          maxLength: 96,
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+      fields: [
+        {
+          name: "alt",
+          title: "Alternative Text",
+          type: "string",
         },
+      ],
+      options: {
+        hotspot: true,
       },
-      {
-        name: "image",
-        title: "Image",
-        type: "image",
-        fields: [
-          {
-            name: "alt",
-            title: "Alternative Text",
-            type: "string",
-          },
-        ],
-        options: {
-          hotspot: true,
-        },
-      },
-    ],
-  };
-  
+    },
+  ],
+};
